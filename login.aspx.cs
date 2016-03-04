ad2edbc1573f8e5ed094a700bef2ed87f0751fdf
Mansoure <memo.nowq@gmail.com>
2016-03-05 00:40:49 +0300

    add register and customer data and add google map and relate with customer

---
+++ login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnlogin_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (tbid.Text == "")
+            {
+                lblerr.Text = "Please input user ID";
+                return;
+            }
+            if (tbpass.Text == "")
+            {
+                lblerr.Text = "Please input user password";
+                return;
+            }
+            Response.Redirect("~\\adminhome.aspx");
+        }
+        catch (Exception ex)
+        {
+
+            lblerr.Text = ex.Message;
+        }
+    }
+}
